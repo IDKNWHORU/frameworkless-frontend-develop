b77a4badf03e003dfe85e40017b5985351d40954
rjh <jh.ra@kisos.net>
2022-11-02 16:00:58 +0900

    add event handler

--- registry.js
+++ registry.js
@@ -2,20 +2,20 @@ const registry = {
 }
 
 const renderWrapper = component => {
-    return (targetElement, state) => {
-        const element = component(targetElement, state);
+    return (targetElement, state, events) => {
+        const element = component(targetElement, state, events);
         const childComponents = element.querySelectorAll("[data-component]");
-        
+
         Array.from(childComponents).forEach(target => {
             const name = target.dataset.component;
-            
+
             const child = registry[name];
-            
+
             if (!child) {
                 return;
             }
 
-            target.replaceWith(child(target, state));
+            target.replaceWith(child(target, state, events));
         });
 
         return element;
@@ -26,12 +26,12 @@ const add = (name, component) => {
     registry[name] = renderWrapper(component);
 }
 
-const renderRoot = (root, state) => {
+const renderRoot = (root, state, events) => {
     const cloneComponent = root => {
-        return root.cloneNode(true)
+        return root.cloneNode(true);
     }
 
-    return renderWrapper(cloneComponent)(root, state)
+    return renderWrapper(cloneComponent)(root, state, events);
 }
 
 export default {
--- view/todos.js
+++ view/todos.js
@@ -8,7 +8,7 @@ const createNewTodoNode = () => {
   return template.content.firstElementChild.cloneNode(true);
 }
 
-const getTodoElement = (todo) => {
+const getTodoElement = (todo, index, events) => {
   const { text, completed } = todo;
 
   const element = createNewTodoNode();
@@ -21,15 +21,19 @@ const getTodoElement = (todo) => {
     element.querySelector('input.toggle').checked = true;
   }
 
+  const handler = e => events.deleteItem(index);
+
+  element.querySelector('button.destroy').addEventListener('click', handler);
+
   return element;
 };
 
-export default (targetElement, { todos }) => {
+export default (targetElement, { todos }, events) => {
   const newTodos = targetElement.cloneNode(true);
 
   newTodos.innerHTML = '';
 
-  todos.map(getTodoElement).forEach(element => {
+  todos.map((todo, index) => getTodoElement(todo, index, events)).forEach(element => {
     newTodos.appendChild(element);
   });
 
