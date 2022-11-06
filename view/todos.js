68e32f41652f415f10fc084605b66163337c63ec
jrh <junho9341@gmail.com>
2022-11-06 09:51:03 +0900

    add delete button event delegation

--- view/todos.js
+++ view/todos.js
@@ -8,7 +8,7 @@ const createNewTodoNode = () => {
   return template.content.firstElementChild.cloneNode(true);
 }
 
-const getTodoElement = (todo, index, events) => {
+const getTodoElement = (todo, index) => {
   const { text, completed } = todo;
 
   const element = createNewTodoNode();
@@ -21,21 +21,25 @@ const getTodoElement = (todo, index, events) => {
     element.querySelector('input.toggle').checked = true;
   }
 
-  const handler = e => events.deleteItem(index);
-
-  element.querySelector('button.destroy').addEventListener('click', handler);
+  element.querySelector('button.destroy').dataset.index = index;
 
   return element;
 };
 
-export default (targetElement, { todos }, events) => {
+export default (targetElement, { todos }, { deleteItem}) => {
   const newTodos = targetElement.cloneNode(true);
 
   newTodos.innerHTML = '';
 
-  todos.map((todo, index) => getTodoElement(todo, index, events)).forEach(element => {
+  todos.map((todo, index) => getTodoElement(todo, index)).forEach(element => {
     newTodos.appendChild(element);
   });
+
+  newTodos.addEventListener('click', e => {
+    if(e.target.matches('button.destroy')){
+      deleteItem(e.target.dataset.index);
+    }
+  })
 
   return newTodos;
 }
